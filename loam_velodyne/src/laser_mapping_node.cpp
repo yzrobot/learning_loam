cceb617533935013dab98b204a978586adf9e38b
Unknown <willemin.florent@gmail.com>
2018-04-08 17:36:49 +0200

    Add Debug step with gdb in new .launch file
    Use new "run & compile" with "loam_velodyne_gdb.launch" file to enable gdb

--- loam_velodyne/src/laser_mapping_node.cpp
+++ loam_velodyne/src/laser_mapping_node.cpp
@@ -9,7 +9,6 @@ int main(int argc, char **argv)
   ros::NodeHandle node;
   ros::NodeHandle privateNode("~");
 
-  loam::LaserMapping laserMapping(0.1);
 
   if (laserMapping.setup(node, privateNode)) {
     // initialization successful
